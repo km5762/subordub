d674a850ef56a7f93d8bb029578690290b3fc311
km5762 <kumyles@gmail.com>
2023-12-18 18:17:51 -0500

    Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-gray-200 font-black text-2xl p-6">
+      <h1>
+        Sub <span className="text-pink-100">|</span> Dub
+      </h1>
+    </header>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      pink: {
+        100: "#FF4D6D",
+        200: "#ff839b",
+        300: "#ffedff",
+      },
+      rose: {
+        100: "#FFAFCC",
+        200: "#98516d",
+      },
+      white: {
+        100: "#FFFFFF",
+        200: "#e0e0e0",
+      },
+      gray: {
+        100: "#1A1A1A",
+        200: "#292929",
+        300: "#404040",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
