45c12e4fcdfe667e55ce5b2a3b222569c465eaa7
km5762 <kumyles@gmail.com>
2023-12-19 13:14:05 -0500

    Get rid of custom color palette, we just using tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="bg-gray-200 font-black text-2xl p-6">
-      <h1>
-        Sub <span className="text-pink-100">|</span> Dub
+    <header className="bg-zinc-800 p-6 flex justify-between items-center">
+      <h1 className="font-black text-2xl">
+        Sub <span className="text-pink-600">|</span> Dub
       </h1>
+      <button className="text-lg font-bold bg-pink-600 hover:bg-pink-700 rounded-full px-4 py-2">
+        Sign in
+      </button>
     </header>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,28 +6,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    colors: {
-      pink: {
-        100: "#FF4D6D",
-        200: "#ff839b",
-        300: "#ffedff",
-      },
-      rose: {
-        100: "#FFAFCC",
-        200: "#98516d",
-      },
-      white: {
-        100: "#FFFFFF",
-        200: "#e0e0e0",
-      },
-      gray: {
-        100: "#1A1A1A",
-        200: "#292929",
-        300: "#404040",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
 export default config;
