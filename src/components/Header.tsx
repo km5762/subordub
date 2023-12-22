efca59bb30ca02c3c059d55847201c0e032c0179
km5762 <kumyles@gmail.com>
2023-12-22 16:07:10 -0500

    Logo in header now navigates back to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Header() {
   return (
     <header className="bg-zinc-800/75 p-6 flex justify-between items-center sticky top-0 z-30 backdrop-blur-2xl shadow-xl">
-      <h1 className="font-black text-2xl">
-        <span className="text-orange-600">Sub</span>
-        <span className="">||</span>
-        <span className="text-purple-600">Dub</span>
-      </h1>
+      <Link href={"/"}>
+        <h1 className="font-black text-2xl">
+          <span className="text-orange-600">Sub</span>
+          <span className="">||</span>
+          <span className="text-purple-600">Dub</span>
+        </h1>
+      </Link>
       <button className="text-lg font-bold bg-pink-600 hover:bg-pink-700 rounded-full px-4 py-2">
         Sign in
       </button>
