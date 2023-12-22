378abfe53ffe690432ae54cc0901f520e2ada830
km5762 <kumyles@gmail.com>
2023-12-22 18:30:19 -0500

    Add blur to card outline

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -17,16 +17,22 @@ export default function ContentCard({
     <Link
       href={"/shows/1"}
       className={
-        "inline-block bg-zinc-800 mr-4 last:mr-0 relative w-40 h-60 rounded-lg p-1 cursor-pointer shadow-xl hover:shadow-xl border-zinc-700 border-solid " +
+        "inline-block bg-zinc-800/75 mr-4 last:mr-0 relative w-40 h-60 rounded-lg p-1 cursor-pointer shadow-xl border-zinc-700 border-solid border overflow-hidden hover:shadow-lg " +
         className
       }
     >
+      <Image
+        src={image}
+        alt={"Content card"}
+        fill
+        className="object-cover rounded-lg -z-10 bg-white blur-sm"
+      />
       <div className="absolute inset-1 bottom-12">
         <Image
           src={image}
           alt={"Content card"}
           fill
-          className="object-cover rounded-lg"
+          className="object-cover rounded-lg border-zinc-700 border-solid border"
         />
       </div>
       <span className="absolute bottom-0 font-black text-3xl">
