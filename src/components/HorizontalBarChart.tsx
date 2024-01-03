f2be0b37b158a5f85955b3e857691858e310bba2
km5762 <kumyles@gmail.com>
2024-01-03 13:18:02 -0500

    Add some cool stripes to the bars in the horizontal bar chart

--- src/components/HorizontalBarChart.tsx
+++ src/components/HorizontalBarChart.tsx
@@ -23,10 +23,16 @@ export default function HorizontalBarChart({
 
   return (
     <div className="flex">
-      <Bar percentage={subPercentage} className="bg-orange-600 rounded-s-lg">
+      <Bar
+        percentage={subPercentage}
+        className="bg-orange-600 rounded-s-lg border-solid border-2 border-orange-500"
+      >
         {labels?.sub}
       </Bar>
-      <Bar percentage={dubPercentage} className="bg-purple-600 rounded-e-lg">
+      <Bar
+        percentage={dubPercentage}
+        className="bg-purple-600 rounded-e-lg border-solid border-2 border-purple-500"
+      >
         {labels?.dub}
       </Bar>
     </div>
@@ -48,7 +54,12 @@ function Bar({
         "h-8 text-center hover:scale-y-125 ease-in-out duration-300 flex items-center justify-center group " +
         className
       }
-      style={{ width: `${percentage}%` }}
+      style={{
+        width: `${percentage}%`,
+        backgroundImage:
+          "linear-gradient(45deg, transparent 25%, rgba(255, 255, 255, 0.1) 25%, rgba(255, 255, 255, 0.1) 50%, transparent 50%, transparent 75%, rgba(255, 255, 255, 0.1) 75%, rgba(255, 255, 255, 0.1) 100%)",
+        backgroundSize: "20px 20px",
+      }}
     >
       <div className="group-hover:scale-x-125 ease-in-out duration-300 ">
         {children}
